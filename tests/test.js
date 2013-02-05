09aa11c4192ae52134c68c0632d10d2243953a77
Reuven V. Gonzales <reuven@virtru.com>
2013-02-05 16:28:14 -0500

    Fixed the as[Type] conversion for own type

--- tests/test.js
+++ tests/test.js
@@ -19,6 +19,12 @@ describe('Binary', function() {
   });
 
   describe('#asByteArray', function() {
+    it('works with a byteArray', function() {
+      var byteArray = [97, 98, 99];
+      var bin = Binary.fromByteArray(byteArray);
+      assert.deepEqual(bin.asByteArray(), byteArray);
+    });
+
     it('works with a string', function() {
       var bin = Binary.fromString('abc');
       var byteArray = bin.asByteArray();
@@ -34,6 +40,11 @@ describe('Binary', function() {
   });
 
   describe('#asString', function() {
+    it('works with a string', function() {
+      var str = 'hello';
+      var bin = Binary.fromString(str);
+      assert.equal(bin.asString(), str);
+    });
     it('works with a byteArray', function() {
       var byteArray = [97, 98, 99];
       var bin = Binary.fromByteArray(byteArray);
@@ -49,6 +60,21 @@ describe('Binary', function() {
   });
 
   describe('#asArrayBuffer', function() {
+    it('works with an arrayBuffer', function() {
+      var byteArray = [97, 98, 99];
+      var bufferView = new Uint8Array(byteArray);
+      var bin = Binary.fromArrayBuffer(bufferView.buffer);
+      
+      var actualBuffer = bin.asArrayBuffer();
+      var bufferView = new Uint8Array(actualBuffer);
+      // Check that the lengths match
+      assert.equal(bufferView.length, byteArray.length);
+      // Check each item
+      for(var i = 0; i < bufferView.length; i++) {
+        assert.equal(bufferView[i], byteArray[i]);
+      }
+    });
+
     it('works with a string', function() {
       var str = 'abc';
       var bin = Binary.fromString(str);

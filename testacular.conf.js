e23b0a67be497045e26fe95c6a69cdac2a064ad3
Reuven V. Gonzales <reuven@virtru.com>
2013-02-15 12:05:02 -0500

    Add chrome canary

--- testacular.conf.js
+++ testacular.conf.js
@@ -58,7 +58,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome', 'Firefox', 'Safari', 'Opera'];
+browsers = ['Chrome', 'ChromeCanary', 'Firefox', 'Safari', 'Opera'];
 
 
 // If browser does not capture in given timeout [ms], kill it
